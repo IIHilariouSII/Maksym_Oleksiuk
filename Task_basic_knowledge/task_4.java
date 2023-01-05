9ad2eed4b10d7892c6f2547ad7cdd67d05c50822
IIHilariouSII <116337736+IIHilariouSII@users.noreply.github.com>
2023-01-05 11:55:52 +0200

    Add files via upload

--- Task_basic_knowledge/task_4.java
+++ Task_basic_knowledge/task_4.java
@@ -1,5 +1,6 @@
+import java.util.Arrays;
 public class task_4 {
-    public static void chkPair(int A[], int size, int x, int count)
+    public static void chkPair_for(int A[], int size, int x, int count)
     {
         for (int i = 0; i < (size - 1); i++) {
             for (int j = (i + 1); j < size; j++) {
@@ -8,7 +9,11 @@ public class task_4 {
                 }
             }
         }
-        System.out.println("Amount of pairs: " + count);
+        System.out.println("Amount of pairs (for): " + count);
+    }
+
+    public static int chkPair_stream(int A[], int x){
+        return (int)Arrays.stream(A).flatMap(i -> Arrays.stream(A).filter(j -> i + j == x )).count() / 2;
     }
  
     public static void main(String[] args)
@@ -19,8 +24,8 @@ public class task_4 {
         int size = A.length;
         int count = 0;
  
-        chkPair(A, size, x, count);
-         
+        chkPair_for(A, size, x, count);
+        System.out.println("Amount of pairs (stream): " + chkPair_stream(A, x));
     } 
 }
 
